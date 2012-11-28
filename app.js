88524de4fb44d7f4de59c4c2aa74cbd9712082e7
Jeong, Heon <blmarket@gmail.com>
2012-11-28 11:24:29 +0900

    Serve public files

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port);
 console.log("Listening on " + port);
